62c59cb0d2c8f8a96684ab5bd905b173ee9967ab
Bruno Marinho <brunoms2083@gmail.com>
2026-06-22 01:06:10 -0300

    atualizando conexão com backend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "https://maravilhas-da-roca.onrender.com",
+  baseURL: "https://maravilhas-da-roca-tiapn.onrender.com",
   withCredentials: true
 })
 
